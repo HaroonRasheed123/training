87639777a125e159259172c592e67c542e3c78e9
Haroon Rasheed <haroon.rasheed@experionglobal.local>
2022-12-29 11:36:02 +0530

    second commit

---
+++ 28-12-2022/program.js
@@ -0,0 +1,23 @@
+console.log(`Hello World`);
+console.log(`Hello`,`World`);
+const i = 5;
+console.log(i);
+var firstName = `John`;
+var lastName = `Doe`;
+console.log(firstName,lastName);
+console.log(typeof firstName);
+var data = `This is a string`;
+console.log(data.length);
+console.log(data[data.length - 1]);
+var res = data.toUpperCase();
+console.log(res);
+var message = `   js for web  `;
+var re = message
+.toUpperCase()
+.trim()
+.replaceAll(` ` , `_`);
+console.log(re);
+const num1 = 4;
+const num2 = 5;
+const resu = num1 + num2;
+console.log(resu);
